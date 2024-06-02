08fb1803be2f8907be57835065e5af39e80937d8
carolyne-wanjiru <shirogithenduka@gmail.com>
2024-06-02 23:08:17 +0300

    add dropdrown menu

--- src/Pages/Tracks/RegistrationForm.jsx
+++ src/Pages/Tracks/RegistrationForm.jsx
@@ -16,6 +16,13 @@ const RegistrationForm = () =>{
                             <input type="text" name="email" placeholder="Email account" required></input>
                             <input type="text" name="course" placeholder="Course Of Study(current)"required></input>
                             <input type="text" name="year" placeholder="year of study" required></input>
+                            <select className="choices" name="Desired course" id="courses">
+                                <option value="Cyber Security">Cyber Security</option>
+                                <option value="Web Development">Web Development</option>
+                                <option value="UI/UX Design">UI/UX Design</option>
+                                <option value="Power Platform">Power Platform</option>
+                                <option value="Mobile App Dev">Mobile App Dev</option>
+                                </select>
                             <button type="submit" class="btn2">submit</button>
                         </fieldset>      
                     </form>
