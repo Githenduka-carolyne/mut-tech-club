b4790c2359c039597c88d31572dcf4806c385789
carolyne-wanjiru <shirogithenduka@gmail.com>
2024-06-02 09:55:53 +0300

    finalize on the home page

--- src/Pages/Home/AboutPart.jsx
+++ src/Pages/Home/AboutPart.jsx
@@ -1,53 +1,63 @@
-const AboutPart = () =>{
-    return (
-
-      <section class="about-section">
-        <div className="about-textbox">
-          <h2>About us:</h2>
-        </div>
-        <div className="container">
-          <div className="card">
-            <div class="card-details">
-              <h3 class="card-title">MISSION</h3>
-              <p class="card-description">
-                To foster a community of technology enthusiasts who are
-                dedicated to learning, sharing, and innovating in the field of
-                technology. We aim to provide resources, opportunities, and a
-                supportive environment where members can enhance their technical
-                skills, collaborate on projects, and stay abreast of the latest
-                industry trends.
-              </p>
-            </div>
+import { FaLowVision } from "react-icons/fa";
+import { GiFlagObjective } from "react-icons/gi";
+import { GiBullseye } from "react-icons/gi";
+const AboutPart = () => {
+  return (
+    <section class="about-section">
+      <div className="about-textbox">
+        <h2>About us:</h2>
+      </div>
+      <div className="container">
+        <div className="card">
+          <div className="icon-holder">
+            <GiBullseye />
           </div>
-
-          <div className="card">
-            <div class="card-details">
-              <h3 class="card-title">VISION</h3>
-              <p class="card-description">
-                To cultivate an environment where passionate individuals come
-                together to explore and advance the field of technology. We
-                strive to become an inspiration and a hub of excellence where
-                members can unlock their potential and contribute to the global
-                tech ecosystem.
-              </p>
-            </div>
-          </div>
-
-          <div className="card">
-            <div class="card-details">
-              <h3 class="card-title">OBJECTIVE</h3>
-              <p class="card-description">
-                To offer workshops, coding boot camps, and seminars to enhance
-                members' technical skills in areas such as programming,
-                cybersecurity, data science,ets. and to facilitate connections
-                between members and industry professionals through events, guest
-                speaker sessions, and partnerships with tech companies.
-              </p>
-            </div>
+          <div class="card-details">
+            <h3 class="card-title">MISSION</h3>
+            <p class="card-description">
+              To foster a community of technology enthusiasts who are dedicated
+              to learning, sharing, and innovating in the field of technology.
+              We aim to provide resources, opportunities, and a supportive
+              environment where members can enhance their technical skills,
+              collaborate on projects, and stay abreast of the latest industry
+              trends.
+            </p>
           </div>
         </div>
-      </section>
-    );
-       
+
+        <div className="card">
+          <div className="icon-holder">
+            <FaLowVision />
+          </div>
+          <div class="card-details">
+            <h3 class="card-title">VISION</h3>
+            <p class="card-description">
+              To cultivate an environment where passionate individuals come
+              together to explore and advance the field of technology. We strive
+              to become an inspiration and a hub of excellence where members can
+              unlock their potential and contribute to the global tech
+              ecosystem.
+            </p>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="icon-holder">
+            <GiFlagObjective />
+          </div>
+          <div class="card-details">
+            <h3 class="card-title">OBJECTIVE</h3>
+            <p class="card-description">
+              To offer workshops, coding boot camps, and seminars to enhance
+              members' technical skills in areas such as programming,
+              cybersecurity, data science,ets. and to facilitate connections
+              between members and industry professionals through events, guest
+              speaker sessions, and partnerships with tech companies.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default AboutPart;
--- src/Pages/Home/TestimonialPart.jsx
+++ src/Pages/Home/TestimonialPart.jsx
@@ -1,85 +1,111 @@
-const TestimonialPart = () =>{
-    return (
-      <section className="testmonies-section">
-        <div className="testimonial">
-          <h2 className="testimonial-title">Testimonials</h2>
-          <p className="testimonial-description">
-            Our current members and alumni share their personal stories,
-            reflecting on how the tech club has enriched their personal and
-            professional journeys respectively.
-          </p>
-        </div>
+import { IoStarSharp } from "react-icons/io5";
+const TestimonialPart = () => {
+  return (
+    <section className="testmonies-section">
+      <div className="testimonial">
+        <h2 className="testimonial-title">Testimonials</h2>
+        <p className="testimonial-description">
+          Our current members and alumni share their personal stories,
+          reflecting on how the tech club has enriched their personal and
+          professional journeys respectively.
+        </p>
+      </div>
 
-        <div className="testimonial-container">
-          <div className="testimonial-card">
-            <div class="testimonial-details">
-              <img></img>
-              <div className="persona-info">
-                <p class="title">Name</p>
-                <p class="role">Role</p>
-              </div>
-              <div className="testimonie">
-                <p>
-                  Our current members and alumni share their personal stories,
-                  reflecting on how the tech club has enriched their personal
-                  and professional journeys respectively.
-                </p>
-              </div>
+      <div className="testimonial-container">
+        <div className="testimonial-card">
+          <div class="testimonial-details">
+            <img
+              className="profile-pic"
+              src="./src/assets/cybersecurityImg.jpg"
+            ></img>
+            <div className="persona-info">
+              <p class="title">John Doe</p>
+              <p class="role">Web Developer</p>
             </div>
-          </div>
-
-          <div className="testimonial-card">
-            <div class="testimonial-details">
-              <img></img>
-              <div className="persona-info">
-                <p class="title">Name</p>
-                <p class="role">Role</p>
-              </div>
-              <div className="testimonie">
-                <p>
-                  Our current members and alumni share their personal stories,
-                  reflecting on how the tech club has enriched their personal
-                  and professional journeys respectively.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="testimonial-card">
-            <div class="testimonial-details">
-              <img></img>
-              <div className="persona-info">
-                <p class="title">Name</p>
-                <p class="role">Role</p>
-              </div>
-              <div className="testimonie">
-                <p>
-                  Our current members and alumni share their personal stories,
-                  reflecting on how the tech club has enriched their personal
-                  and professional journeys respectively.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="testimonial-card">
-            <div class="testimonial-details">
-              <img></img>
-              <div className="persona-info">
-                <p class="title">Name</p>
-                <p class="role">Role</p>
-              </div>
-              <div className="testimonie">
-                <p>
-                  Our current members and alumni share their personal stories,
-                  reflecting on how the tech club has enriched their personal
-                  and professional journeys respectively.
-                </p>
+            <div className="testimonie">
+              <p>
+                <i>
+                  "Being a part of the web Development track,I gained practical
+                  skills through capture-the-flag competitions and workshops led
+                  by industry experts.The experience not only enhanced my resume
+                  but also gave me confidence in pursuing a career in Web
+                  Development."
+                </i>
+              </p>
+              <div className="stars">
+                <IoStarSharp />
+                <IoStarSharp />
+                <IoStarSharp />
+                <IoStarSharp />
+                <IoStarSharp />
               </div>
             </div>
           </div>
         </div>
-      </section>
-    );
-}
+
+        <div className="testimonial-card">
+          <div class="testimonial-details">
+            <img
+              className="profile-pic"
+              src="./src/assets/cybersecurityImg.jpg"
+            ></img>
+            <div className="persona-info">
+              <p class="title">Dennis Doe</p>
+              <p class="role">Cyber Security(alumni)</p>
+            </div>
+            <div className="testimonie">
+              <p>
+                <i>
+                  "Being a part of the MUT Tech club under track Cybersecurity
+                  Club, I gained practical skills through capture-the-flag
+                  competitions and workshops led by industry experts. The
+                  experience not only enhanced my resume but also gave me
+                  confidence in pursuing a career in cybersecurity."
+                </i>
+              </p>
+              <div className="stars">
+                <IoStarSharp />
+                <IoStarSharp />
+                <IoStarSharp />
+                <IoStarSharp />
+                <IoStarSharp />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="testimonial-card">
+          <div class="testimonial-details">
+            <img
+              className="profile-pic"
+              src="./src/assets/cybersecurityImg.jpg"
+            ></img>
+            <div className="persona-info">
+              <p class="title">Francis Kimani</p>
+              <p class="role">Mobile Developer</p>
+            </div>
+            <div className="testimonie">
+              <p>
+                <i>
+                  "Being a part of the Mobile Development track, I gained
+                  practical skills through capture-the-flag competitions and
+                  workshops led by industry experts. The experience not only
+                  enhanced my resume but also gave me confidence in pursuing a
+                  career in Mobile Development."
+                </i>
+              </p>
+              <div className="stars">
+                <IoStarSharp />
+                <IoStarSharp />
+                <IoStarSharp />
+                <IoStarSharp />
+                <IoStarSharp />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default TestimonialPart;
