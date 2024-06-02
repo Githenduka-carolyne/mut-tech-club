77a56830f206e84e5e2d3cef332c48d6583d9b7e
carolyne-wanjiru <shirogithenduka@gmail.com>
2024-06-02 14:10:04 +0300

    incorporate changes in the leadership page

---
+++ src/Pages/Leadership/Leaderships.jsx
@@ -0,0 +1,265 @@
+import "./Leaderships.css"
+import { FaLinkedin } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaFacebookSquare } from "react-icons/fa";
+
+const Leaderships = () => {
+  return (
+    <div className="leader-container">
+      <div className="card-wrapper">
+        <div className="leader-card-info">
+          <div className="leader-photo">
+            <img src="./src/assets/trial2.jpeg" alt="Dr John Ndia photo" />
+          </div>
+          <div className="social-platforms">
+            <FaLinkedin />
+            <FaSquareXTwitter />
+            <FaFacebookSquare />
+          </div>
+          <div className="leaders-detail">
+            <h3 className="name">Dr. John Ndia</h3>
+            <h3 className="role">Patron</h3>
+          </div>
+          <div>  
+            <p className="bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              odio!. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Neque, odio!. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Neque, odio!.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Neque, odio!
+            </p>
+          </div>
+        </div>
+
+        <div className="leader-card-info-white">
+          <div className="leader-photo">
+            <img src="./src/assets/trial2.jpeg" alt="Dr John Ndia photo" />
+          </div>
+          <div className="social-platforms">
+            <FaLinkedin />
+            <FaInstagramSquare />
+            <FaSquareXTwitter />
+            <FaFacebookSquare />
+          </div>
+          <div className="leaders-detail">
+            <h3 className="name">Victory Njeri</h3>
+            <h3 className="role">Chairperson</h3>
+            <p className="bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              odio!. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Neque, odio!. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Neque, odio!.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Neque, odio!
+            </p>
+          </div>
+        </div>
+
+        <div className="leader-card-info">
+          <div className="leader-photo">
+            <img src="./src/assets/trial2.jpeg" alt="Dr John Ndia photo" />
+          </div>
+          <div className="social-platforms">
+            <FaLinkedin />
+            <FaSquareXTwitter />
+          </div>
+          <div className="leaders-detail">
+            <h3 className="name">Allan Muhari </h3>
+            <h3 className="role">Vice Chairperson</h3>
+            <p className="bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              odio!. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Neque, odio!. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Neque, odio!.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Neque, odio!
+            </p>
+          </div>
+        </div>
+
+        <div className="leader-card-info-white">
+          <div className="leader-photo">
+            <img src="./src/assets/trial2.jpeg" alt="Dr John Ndia photo" />
+          </div>
+          <div className="social-platforms">
+            <FaLinkedin />
+            <FaSquareXTwitter />
+          </div>
+          <div className="leaders-detail">
+            <h3 className="name">Bridget Gitonga</h3>
+            <h3 className="role"> Secretary</h3>
+            <p className="bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              odio!. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Neque, odio!. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Neque, odio!.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Neque, odio!
+            </p>
+          </div>
+        </div>
+
+        <div className="leader-card-info">
+          <div className="leader-photo">
+            <img src="./src/assets/trial2.jpeg" alt="Dr John Ndia photo" />
+          </div>
+          <div className="social-platforms">
+            <FaLinkedin />
+            <FaInstagramSquare />
+          </div>
+          <div className="leaders-detail">
+            <h3 className="name">Ruth Mutisya</h3>
+            <h3 className="role">Treasurer </h3>
+            <p className="bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              odio!. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Neque, odio!. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Neque, odio!.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Neque, odio!
+            </p>
+          </div>
+        </div>
+
+        <div className="leader-card-info-white">
+          <div className="leader-photo">
+            <img src="./src/assets/trial2.jpeg" alt="Dr John Ndia photo" />
+          </div>
+          <div className="social-platforms">
+            <FaLinkedin />
+            <FaInstagramSquare />
+            <FaSquareXTwitter />
+            <FaFacebookSquare />
+          </div>
+          <div className="leaders-detail">
+            <h3 className="name">Webster Ifedha </h3>
+            <h3 className="role">Cyber Security Lead</h3>
+            <p className="bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              odio!. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Neque, odio!. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Neque, odio!.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Neque, odio!
+            </p>
+          </div>
+        </div>
+
+        <div className="leader-card-info">
+          <div className="leader-photo">
+            <img src="./src/assets/trial2.jpeg" alt="Dr John Ndia photo" />
+          </div>
+          <div className="social-platforms">
+            <FaLinkedin />
+            <FaInstagramSquare />
+            <FaSquareXTwitter />
+            <FaFacebookSquare />
+          </div>
+          <div className="leaders-detail">
+            <h3 className="name">Manase Gunga</h3>
+            <h3 className="role">UI/UX Lead</h3>
+            <p className="bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              odio!. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Neque, odio!. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Neque, odio!.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Neque, odio!
+            </p>
+          </div>
+        </div>
+
+        <div className="leader-card-info-white">
+          <div className="leader-photo">
+            <img src="./src/assets/trial2.jpeg" alt="Dr John Ndia photo" />
+          </div>
+          <div className="social-platforms">
+            <FaLinkedin />
+            <FaInstagramSquare />
+            <FaSquareXTwitter />
+            <FaFacebookSquare />
+          </div>
+          <div className="leaders-detail">
+            <h3 className="name">Carolyne Githenduka </h3>
+            <h3 className="role">Web Development Lead</h3>
+            <p className="bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              odio!. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Neque, odio!. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Neque, odio!.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Neque, odio!
+            </p>
+          </div>
+        </div>
+
+        <div className="leader-card-info">
+          <div className="leader-photo">
+            <img src="./src/assets/trial2.jpeg" alt="Dr John Ndia photo" />
+          </div>
+          <div className="social-platforms">
+            <FaLinkedin />
+            <FaInstagramSquare />
+            <FaSquareXTwitter />
+            <FaFacebookSquare />
+          </div>
+          <div className="leaders-detail">
+            <h3 className="name">Stanley Amunze</h3>
+            <h3 className="role"> Mobile Apps Development Lead</h3>
+            <p className="bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              odio!. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Neque, odio!. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Neque, odio!.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Neque, odio!
+            </p>
+          </div>
+        </div>
+
+        <div className="leader-card-info">
+          <div className="leader-photo">
+            <img src="./src/assets/trial2.jpeg" alt="Dr John Ndia photo" />
+          </div>
+          <div className="social-platforms">
+            <FaLinkedin />
+            <FaInstagramSquare />
+            <FaSquareXTwitter />
+            <FaFacebookSquare />
+          </div>
+          <div className="leaders-detail">
+            <h3 className="name">Paul Karanja</h3>
+            <h3 className="role">Cloud Engineering Lead</h3>
+            <p className="bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              odio!. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Neque, odio!. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Neque, odio!.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Neque, odio!
+            </p>
+          </div>
+        </div>
+
+        <div className="leader-card-info">
+          <div className="leader-photo">
+            <img src="./src/assets/trial2.jpeg" alt="Dr John Ndia photo" />
+          </div>
+          <div className="social-platforms">
+            <FaLinkedin />
+            <FaInstagramSquare />
+            <FaSquareXTwitter />
+            <FaFacebookSquare />
+          </div>
+          <div className="leaders-detail">
+            <h3 className="name">Evyonn Mbithe</h3>
+            <h3 className="role">Power Platform Lead.</h3>
+            <p className="bio">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              odio!. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Neque, odio!. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Neque, odio!.Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Neque, odio!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Leaderships;
+
+
+
--- src/Pages/Leadership/leadership.jsx
+++ src/Pages/Leadership/leadership.jsx
@@ -1,12 +1,21 @@
 import Bannerpart from "../../component/Header/Banner/Bannerpart/";
+import Leaderships from "./Leaderships";
+import React from "react";
 const leadership = () =>{
       return (
-        <section className="leadership-section">
+        <React.Fragment>
           <Bannerpart
             maintitle="Our Leadership Team"
             description="Dedicated team behind Murang'a University Tech Club (MUTC)."
-          />
-        </section>
+          /> 
+
+          <section className="leader-section">
+            <Leaderships/>
+          </section>
+        </React.Fragment> 
+        
+        
+        
       );
 }
 export default leadership;
